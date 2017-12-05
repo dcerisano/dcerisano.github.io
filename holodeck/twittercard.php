f4f59d09e6b036a252272ca134fc030c2c2ff55d
dcerisano <dcerisano@standard3d.com>
2017-12-04 21:09:22 -0500

    Signed-off-by: dcerisano <dcerisano@standard3d.com>

--- holodeck/twittercard.php
+++ holodeck/twittercard.php
@@ -1,6 +1,8 @@
 <?php
 $media = json_decode(file_get_contents("media.json"));
-$txt="your first bla";
+$image = "https://www.standard3d.com/3D-VR-Panorama/assets/twittercards/img/" . $media[0];
+$title = $media[1];
+$descr = $media[2];
 ?>
 <HTML>
 <head>
@@ -8,9 +10,9 @@ $txt="your first bla";
 <meta name="twitter:card" content="summary_large_image">
 <meta name="twitter:site" content="@standard3d">
 <meta name="twitter:creator" content="@standard3d">
-<meta name="twitter:title" content="<?php echo $txt ?>">
-<meta name="twitter:description" content="<?php echo $txt ?>">
-<meta name="twitter:image" content="https://www.standard3d.com/3D-VR-Panorama/assets/twittercards/img/10320_9BF975D9-07CA-4CD5-A974-1D016244B9BA_pano.jpg">
+<meta name="twitter:image" content="<?php echo $image ?>">
+<meta name="twitter:title" content="<?php echo $title ?>">
+<meta name="twitter:description" content="<?php echo $descr ?>">
 <meta http-equiv="Pragma" content="no-cache">
 <meta http-equiv="refresh"
 	content="0; url=https://play.google.com/store/apps/details?id=com.standard3d.holodeck.v1.free" />
